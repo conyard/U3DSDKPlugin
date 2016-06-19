61c01d7e2a0c6b5fa66a1e48d1fdc6f745ccd266
Drifter <379657505@qq.com>
2016-06-19 17:08:42 +0800

    添加个方法

--- src/com/sdkplugin/bridge/AbsU3DListener.java
+++ src/com/sdkplugin/bridge/AbsU3DListener.java
@@ -2,6 +2,7 @@ package com.sdkplugin.bridge;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 
 import com.unity3d.player.UnityPlayer;
 
@@ -16,6 +17,10 @@ public abstract class AbsU3DListener implements IU3DListener {
 
 	public Context getCurContext() {
 		return UnityPlayer.currentActivity;
+	}
+	
+	public Intent getCurIntent() {
+		return UnityPlayer.currentActivity.getIntent();
 	}
 
 	@Override
